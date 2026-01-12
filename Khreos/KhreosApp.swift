b785ff7fa44c3e21a3901cb83f2f81968173463a
Vlad <mavrodimakis@icloud.com>
2026-01-12 10:02:30 +0200

    Initial Commit

---
+++ Khreos/KhreosApp.swift
@@ -0,0 +1,17 @@
+//
+//  KhreosApp.swift
+//  Khreos
+//
+//  Created by Vladyslav Mavrodimaki on 12/1/26.
+//
+
+import SwiftUI
+
+@main
+struct KhreosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
